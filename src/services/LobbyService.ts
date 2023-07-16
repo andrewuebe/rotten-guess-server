9907ef6adcccbfcaeb33cff10e479d6692b46b4f
andrewuebe <andrewuebe@gmail.com>
2023-07-16 08:36:36 -0700

    LobbyService changes

--- src/services/LobbyService.ts
+++ src/services/LobbyService.ts
@@ -1,3 +1,8 @@
+import LobbyHelper from "../helpers/LobbyHelper";
+import { LobbyRepository } from '../database/repositories';
+import { PlayerRepository } from '../database/repositories';
+import jwt from 'jsonwebtoken';
+
 export default class LobbyService {
   constructor() { }
 
@@ -5,10 +10,15 @@ export default class LobbyService {
     console.log('lets get the lobby');
     return { lobbyId };
   }
-  ß
+
   async createLobby() {
     console.log('lets create the lobby');
-    return { lobbyId: '123' };
+    const lobbyToken = await LobbyHelper.generateLobbyToken();
+    const newPlayer = await PlayerRepository.newPlayer({ lobbyToken });
+    const playerToAdd = { id: newPlayer._id, name: newPlayer.name };
+    const token = jwt.sign(playerToAdd, process.env.JWT_SECRET, { expiresIn: '1h' })
+    const newLobby = await LobbyRepository.newLobby(lobbyToken, playerToAdd);
+    return { lobby: newLobby, token };
   }
 
   async putLobby(lobbyId: string) {
@@ -16,9 +26,13 @@ export default class LobbyService {
     return { lobbyId };
   }
 
-  async joinLobby(lobbyId: string) {
+  async joinLobbyNewPlayer(lobbyToken: string, playerName?: string) {
     console.log('lets join the lobby');
-    return { lobbyId };
+    const newPlayer = await PlayerRepository.newPlayer({ playerName, lobbyToken });
+    const playerToAdd = { id: newPlayer._id, name: newPlayer.name };
+    const token = jwt.sign(playerToAdd, process.env.JWT_SECRET, { expiresIn: '1h' })
+    const lobby = await LobbyRepository.addPlayerToLobby(lobbyToken, playerToAdd);
+    return { lobby, token };
   }
 
   async deleteLobby(lobbyId: string) {
